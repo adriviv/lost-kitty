a2b49da853ee0154e3f231999b5eae051ae507e1
Adrien Viviani <adrien.viv@gmail.com>
2019-04-02 18:15:36 +0800

    exercise finish but forrest want always more

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,25 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.create!(animal_params)
+    redirect_to animals_path
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:breed, :color, :description)
+  end
+end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,6 @@
 class Animal < ApplicationRecord
+
+  validates :breed, presence: true
+  validates :color, presence: true
+  validates :description, presence: true
 end
