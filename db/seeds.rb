73dda26db6c679ac7c71e0b47694bedb86e1d42f
Adrien Viviani <adrien.viv@gmail.com>
2019-04-02 17:43:07 +0800

    AddSeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+10.times  do
+  Animal.create!(
+    breed: Faker::Creature::Animal.name,
+    color: Faker::Color.color_name,
+    description: Faker::TvShows::FamilyGuy.quote
+    )
+
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
